f938124a6059d375fa1c30e1cfe3e11f10610201
695518147 <695518147@qq.com>
2019-01-27 11:44:28 +0800

    url

--- xiaobing/urls.py
+++ xiaobing/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from xiaobing import views
 
 urlpatterns = [
-
+    path('', views.index),
     path('search/', views.search),
     path('edit/', views.edit),
     path('saveOrder/', views.saveOrder),
